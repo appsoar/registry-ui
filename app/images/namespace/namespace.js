11688a6a5d617ee6e1e80416090fc14c092b1000
liweirar <77111086@qq.com>
2016-03-23 09:20:11 +0800

    修改命名空间

--- app/images/namespace/namespace.js
+++ app/images/namespace/namespace.js
@@ -5,70 +5,38 @@
 
 var namespaceApp = angular.module('registryUiApp');
 
-namespaceApp.controller('namespaceCtrl', ['$scope', '$filter', 'ngTableParams', '$resource', '$timeout', 'ngTableDataService','namespaceService',
-    function($scope, $filter, ngTableParams, $resource, $timeout, ngTableDataService, namespaceService) {
-    var vm = this;
-
-    //var test=namespaceService.query();
-    //console.log(test)
-
-
-    var Api = $resource('images/namespace/table-data.json');
-
-    vm.tableParams5 = new ngTableParams({
-        page: 1,            // show first page
-        count: 10           // count per page
-    }, {
-        total: 0,           // length of data
-        counts: [],         // hide page counts control
-        getData: function($defer, params) {
-
-            // Service using cache to avoid mutiple requests
-            ngTableDataService.getData( $defer, params, Api);
-
-
-        }
-    });
-}]);
-
-namespaceApp.controller('namespaceEditCtrl',['$scope', '$location','$stateParams', function($scope,   $location, $stateParams){
-    var vm = this;
-  // console.log($stateParams.id);
-    var namespace= {
-        id: $stateParams.id,
-        Namespace:"",
-        Type:"",
-        ImageNum:"",
-        Date:"",
-        PullNum:""
-    };
-    vm.namespace=namespace;
-
-    //判断参数是否存在ID
-    if($stateParams.id){
-        console.log($stateParams.id)
-        namespace.Namespace = "测试";
-    }
-    else{
-        console.log('add')
-    }
-}]);
-
-namespaceApp.controller('namespaceViewCtrl', ['$scope', function($scope){
-
-}]);
+namespaceApp.controller('namespaceCtrl', ['$scope', '$filter', 'ngTableParams', '$resource', '$timeout', 'ngTableDataService', 'namespaceListService',
+    function ($scope, $filter, ngTableParams, $resource, $timeout, ngTableDataService, namespaceListService) {
+        var vm = this;
+        vm.resp = namespaceListService.query();
+        
+        //console.log(vm.resp)
+        vm.tableParams = new ngTableParams({
+            page: 1,
+            count: 10,
+            sorting: {
+                _id: 'asc'
+            }
+        }, {
+            total: 0,
+            counts: [],
+            getData: function ($defer, params) {
+                ngTableDataService.getData($defer, params);
+            }
+        });
+    }]);
 
 
-// NOTE: We add the service definition here for quick reference
-namespaceApp.service('ngTableDataService', function() {
 
+namespaceApp.service('ngTableDataService', ['namespaceListService', '$filter', function (namespaceListService, $filter) {
     var TableData = {
         cache: null,
-        getData: function($defer, params, api){
+        getData: function ($defer, params) {
             // if no cache, request data and filter
-            if ( ! TableData.cache ) {
-                if ( api ) {
-                    api.get(function(data){
+            if (!TableData.cache) {
+                var test = namespaceListService.query();
+                if (test) {
+                    namespaceListService.query(function (data) {
                         TableData.cache = data;
                         filterdata($defer, params);
                     });
@@ -79,9 +47,12 @@ namespaceApp.service('ngTableDataService', function() {
             }
 
             function filterdata($defer, params) {
+                var orderedData = params.sorting() ?
+                    $filter('orderBy')(TableData.cache.result, params.orderBy()) : TableData.cache.result;
+
                 var from = (params.page() - 1) * params.count();
                 var to = params.page() * params.count();
-                var filteredData = TableData.cache.result.slice(from, to);
+                var filteredData = orderedData.slice(from, to);
 
                 params.total(TableData.cache.total);
                 $defer.resolve(filteredData);
@@ -91,8 +62,50 @@ namespaceApp.service('ngTableDataService', function() {
     };
 
     return TableData;
+}]);
 
-});
+namespaceApp.controller('namespaceEditCtrl', ['$scope', '$location', '$stateParams', function ($scope, $location, $stateParams) {
+    var vm = this;
+    vm.selectModle = [
+        { name: '公有', value: 'public' },
+        { name: '私有', value: 'private' }
+    ];
+    vm.selected = "public";
+    vm.selectedChange = function () {
+        alert(vm.selected)
+    }
 
+    // console.log($stateParams.id);
+    vm.namespace = {
+        id: $stateParams.id,
+        Namespace: "",
+        Type: "",
+        ImageNum: "",
+        Date: "",
+        PullNum: ""
+    };
+    // vm.namespace=namespace;
 
+    //判断参数是否存在ID
+    if ($stateParams.id) {
+        console.log($stateParams.id)
+        vm.namespace.Namespace = "测试";
+    }
+    else {
+        console.log('add')
+    }
+}]);
 
+namespaceApp.controller('namespaceViewCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
+    //alert($stateParams._id)
+    var vm = this;
+    vm.namespace = {
+        _id: $stateParams._id,
+        permission: 'public',
+        create_time: new Date(),
+        owner_id: 'admin',
+        desc: '123',
+        imgNum: 100,
+    };
+
+}]);
--- app/services/services/NamespaceService.js
+++ app/services/services/NamespaceService.js
@@ -3,32 +3,48 @@
  */
 'use strict'
 
-angular.module('registryUiApp')
-    .factory('namespaceService', ['$resource', function($resource)
+var namespaceApp = angular.module('registryUiApp');
+var url = "http://192.168.12.112:8080";
+namespaceApp.factory('namespaceListService', ['$resource', function($resource)
     {
-        return $resource('/v2/tabledata', {}, {
-            'query':{
+        //{"content": [{"_id": "appsoar", "create_time": 1458301228013.0, "owner_id": "admin", "desc": ""}], "message": "done", "result": 0}
+        return $resource(url+'/api/namespaces', {}, {
+            'query': {
                 method:'GET',
-                isArray:false,
-                transformResponse:function(data, headers){
-                    var resp = angular.fromJson(data);
-                    var ret = {
-                        result: [],
-                        total: resp.total
-                    };
-                    angular.forEach(resp.result, function(item){
-                        ret.result.push({
-                            Namespace: item.Namespace,
-                            Type:item.Type,
-                            ImageNum:item.ImageNum,
-                            Date:item.Date,
-                            PullNum:item.PullNum
+                isArray: false,
+                transformResponse: function(data, headers){
+                    //console.log(data)
+                    var repos = angular.fromJson(data);
+
+                    //console.log(repos.length)
+                    if(repos.result == "0") {
+                        var content = repos.content;
+                        var ret = {
+                            result: [],
+                            total: content.length
+                        };
+                        angular.forEach(content, function (value/*, key*/) {
+                            ret.result.push({
+                                _id : value._id,
+                                permission : value.permission,
+                                create_time : new Date(value.create_time),
+                                owner_id : value.owner_id,
+                                desc : value.desc,
+                                imgNum : value.repo_num
+                            });
                         });
-                    });
-                    return ret;
+                        return ret;
+                    }
+                    else{
+                        console.log(repos.message);
+                    }
                 }
             }
         })
-    }])
+    }]);
+
+namespaceApp.factory('namespaceService', ['$resource', function($resource){
+
+}]);
 
 
