bc29208820a0e45e4246186d4b011ea6219d0b31
liweirar <77111086@qq.com>
2016-03-24 17:34:57 +0800

    update namespace

---
+++ app/services/services/DashboardStatisticService.js
@@ -0,0 +1,43 @@
+/**
+ * Created by liwei on 2016/3/24.
+ */
+'use strict'
+
+angular.module('registryUiApp').factory('dashboardStatisticService', function($websocket, wsdashboardStatisticUrl) {
+    // Open a WebSocket connection
+    var ws = $websocket(wsdashboardStatisticUrl);
+    var collection = [] ;
+    ws.onError(function (event) {
+        console.error('connection Error', event);
+    });
+    ws.onClose(function (event) {
+        //console.log('connection closed', event);
+    });
+    ws.onOpen(function () {
+        //console.log('connection open');
+    });
+    ws.onMessage(function(event) {
+        // var response;
+        try {
+            collection[0] = angular.fromJson(event.data).content;
+            console.log(collection)
+        } catch (e) {
+            console.log('error: ', e);
+            // response = {'error': e};
+        }
+    });
+    return {
+        collection: collection,
+        status: function () {
+            return ws.readyState;
+        },
+        send: function (message) {
+            if (angular.isString(message)) {
+                ws.send(message);
+            }
+            else if (angular.isObject(message)) {
+                ws.send(JSON.stringify(message));
+            }
+        }
+    };
+});
